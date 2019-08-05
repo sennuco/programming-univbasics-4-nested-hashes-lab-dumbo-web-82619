dd9431503bc1c6ca7d16143bfa303a9f151d8348
Theresa Cyrus <cyrustheresa@gmail.com>
2019-08-05 19:57:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -14,10 +14,9 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
-
-end
+     } 
+     programmer_hash[:grace_hopper]
+  end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,9 +55,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
+  
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
@@ -65,8 +67,11 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{
-        :grace_hopper => {
+ 		{   :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+         :languages => ["LISP", "C"]
+ 		},
+         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
@@ -79,14 +84,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
     
 end
 
 def changing_alan
 	programmer_hash = 
  		{
-        :grace_hopper => {
+          :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
@@ -99,9 +103,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+    programmer_hash[:alan_kay][:known_for]="GUI"
+    programmer_hash
      
 end
 
@@ -121,5 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
